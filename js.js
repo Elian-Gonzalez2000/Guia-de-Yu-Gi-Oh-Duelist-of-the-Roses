1160b9eacac4a0e594c3b68dca37741414cd8c0a
Elian <eliancarlogm@gmail.com>
2012-11-02 01:44:52 -0430

    Añado scroll spy, añado estilos nuevos, separo texto html, etc

--- js.js
+++ js.js
@@ -1,20 +1,48 @@
-const d = document,
-	$menuBtn = d.querySelector(".menu-btn"),
-	$menuNav = d.querySelector(".menu-nav");
+const d = document;
 
-	function displayMenu(panelBtn, panel, menuLinks){
-		d.addEventListener("click", (e) => {
-			if(e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)){
-				d.querySelector(panel).classList.toggle("show");
-				d.querySelector(panelBtn).classList.toggle("show");
-            d.querySelector(panelBtn).classList.toggle("active-btn");
+function displayMenu(panelBtn, panel, menuLinks) {
+  d.addEventListener("click", (e) => {
+    if (e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)) {
+      d.querySelector(panel).classList.toggle("show");
+      d.querySelector(panelBtn).classList.toggle("show");
+      d.querySelector(panelBtn).classList.toggle("active-btn");
+    }
 
-			}
+    if (e.target.matches(menuLinks) || e.target.matches(`${menuLinks} *`)) {
+      d.querySelector(panel).classList.remove("show");
+      d.querySelector(panelBtn).classList.remove("active-btn");
+    }
+  });
+}
 
-			if(e.target.matches(menuLinks) || e.target.matches(`${menuLinks} *`)){
-				d.querySelector(panel).classList.remove("show");
-			}
-		})
-	}
+function scrollSpy() {
+  const d = document;
+  const $section = d.querySelectorAll("section[data-scroll-spy]");
+  const cb = (entries) => {
+    //console.log("entries", entries);
 
-displayMenu(".menu-btn", ".menu-nav", ".menu-nav a");
+    entries.forEach((entry) => {
+      //console.log("entry", entry);
+      const id = entry.target.getAttribute("id");
+      //console.log(id);
+
+      if (entry.isIntersecting) {
+        d.querySelector(`a[href="#${id}"]`).classList.add("active");
+      } else {
+        d.querySelector(`a[href="#${id}"]`).classList.remove("active");
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(cb, {
+    //root
+    //rootMargin
+    threshold: [0.4, 0.6],
+  });
+
+  $section.forEach((el) => observer.observe(el));
+}
+d.addEventListener("DOMContentLoaded", (e) => {
+  displayMenu(".menu-btn", ".menu-nav", ".menu-nav a");
+  scrollSpy();
+});
