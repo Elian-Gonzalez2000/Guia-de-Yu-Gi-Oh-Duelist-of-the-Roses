85c3ac7e25142047f1e60c05c3bbd26b9ae4a0ee
Elian <eliancarlogm@gmail.com>
2021-02-06 10:54:55 -0430

    Agruegue el codigo del menu hamburguesa

---
+++ js.js
@@ -0,0 +1,18 @@
+const d = document,
+	$menuBtn = d.querySelector(".menu-btn"),
+	$menuNav = d.querySelector(".menu-nav");
+
+	function displayMenu(panelBtn, panel, menuLinks){
+		d.addEventListener("click", (e) => {
+			if(e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)){
+				d.querySelector(panel).classList.toggle("show");
+				d.querySelector(panelBtn).classList.toggle("show");
+			}
+
+			if(e.target.matches(menuLinks) || e.target.matches(`${menuLinks} *`)){
+				d.querySelector(panel).classList.remove("show");
+			}
+		})
+	}
+
+displayMenu(".menu-btn", ".menu-nav", ".menu-nav a");
